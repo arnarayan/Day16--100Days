24f6c0e58d43d4cf45f0bac30b9383f569fb9767
Anand.Narayan <arnarayan@gmail.com>
2022-10-10 11:21:35 -0600

    Initial Commit

---
+++ Day16--100Days/Day16__100DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day16__100DaysApp.swift
+//  Day16--100Days
+//
+//  Created by Anand Narayan on 2022-10-10.
+//
+
+import SwiftUI
+
+@main
+struct Day16__100DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
